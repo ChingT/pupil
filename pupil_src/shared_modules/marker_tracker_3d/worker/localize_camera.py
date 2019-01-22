0115d9d6a6bf8a19c0e2c137b0754e3509b51513
ChingT <j60626j@gmail.com>
2019-01-22 17:45:21 +0100

    fix bug: camera_extrinsics becomes None if the camera moves too much

--- pupil_src/shared_modules/marker_tracker_3d/worker/localize_camera.py
+++ pupil_src/shared_modules/marker_tracker_3d/worker/localize_camera.py
@@ -3,7 +3,6 @@ import numpy as np
 from marker_tracker_3d import worker
 
 min_n_markers_per_frame = 1
-max_camera_trace_distance = 10
 
 
 def localize(
@@ -15,9 +14,6 @@ def localize(
     data_for_solvepnp = _prepare_data_for_solvepnp(
         marker_id_to_detections, marker_id_to_extrinsics_prv
     )
-    if not data_for_solvepnp:
-        return None
-
     camera_extrinsics = _calculate(
         camera_intrinsics, data_for_solvepnp, camera_extrinsics_prv
     )
@@ -49,42 +45,51 @@ def _prepare_data_for_solvepnp(marker_id_to_detections, marker_id_to_extrinsics_
 
 
 def _calculate(camera_intrinsics, data_for_solvepnp, camera_extrinsics_prv):
+    if not data_for_solvepnp:
+        return None
+
     markers_points_3d, markers_points_2d = data_for_solvepnp
+
     retval, rotation, translation = _run_solvepnp(
         camera_intrinsics, markers_points_3d, markers_points_2d, camera_extrinsics_prv
     )
 
-    if _check_solvepnp_output_reasonable(
+    if not _check_solvepnp_output_reasonable(
         retval, rotation, translation, markers_points_3d
     ):
-        camera_extrinsics = worker.utils.merge_extrinsics(rotation, translation)
-        if _check_camera_trace_reasonable(camera_extrinsics, camera_extrinsics_prv):
-            return camera_extrinsics
+        retval, rotation, translation = _run_solvepnp(
+            camera_intrinsics, markers_points_3d, markers_points_2d
+        )
+        if not _check_solvepnp_output_reasonable(
+            retval, rotation, translation, markers_points_3d
+        ):
+            return None
 
-    return None
+    camera_extrinsics = worker.utils.merge_extrinsics(rotation, translation)
+    return camera_extrinsics
 
 
 def _run_solvepnp(
-    camera_intrinsics, markers_points_3d, markers_points_2d, frame_id_to_extrinsics_prv
+    camera_intrinsics, markers_points_3d, markers_points_2d, camera_extrinsics_prv=None
 ):
     assert len(markers_points_3d) == len(markers_points_2d)
     assert markers_points_3d.shape[1:] == (4, 3)
     assert markers_points_2d.shape[1:] == (4, 2)
 
-    if frame_id_to_extrinsics_prv is None:
+    if camera_extrinsics_prv is None:
         retval, rotation, translation = camera_intrinsics.solvePnP(
             markers_points_3d, markers_points_2d
         )
     else:
         rotation_prv, translation_prv = worker.utils.split_extrinsics(
-            frame_id_to_extrinsics_prv
+            camera_extrinsics_prv
         )
         retval, rotation, translation = camera_intrinsics.solvePnP(
             markers_points_3d,
             markers_points_2d,
             useExtrinsicGuess=True,
-            rvec=rotation_prv,
-            tvec=translation_prv,
+            rvec=rotation_prv.copy(),
+            tvec=translation_prv.copy(),
         )
     return retval, rotation, translation
 
@@ -107,26 +112,7 @@ def _check_solvepnp_output_reasonable(retval, rotation, translation, pts_3d_worl
     pts_3d_camera = worker.utils.to_camera_coordinate(
         pts_3d_world, rotation, translation
     )
-    if (pts_3d_camera[:, 2] < 0).any():
+    if (pts_3d_camera[:, 2] < 1).any():
         return False
 
     return True
-
-
-def _check_camera_trace_reasonable(camera_extrinsics, camera_extrinsics_prv):
-    # If one of camera_extrinsics and camera_extrinsics_prv is None,
-    # then just ignore this checking step.
-    if camera_extrinsics is None or camera_extrinsics_prv is None:
-        return True
-
-    # If the camera position is too far from the previous camera position,
-    # then it is very likely the output from solvePnP is wrong estimation.
-    camera_trace = worker.utils.get_camera_trace_from_extrinsics(camera_extrinsics)
-    camera_trace_prv = worker.utils.get_camera_trace_from_extrinsics(
-        camera_extrinsics_prv
-    )
-    camera_trace_distance = np.linalg.norm(camera_trace - camera_trace_prv)
-    if camera_trace_distance > max_camera_trace_distance:
-        return False
-    else:
-        return True

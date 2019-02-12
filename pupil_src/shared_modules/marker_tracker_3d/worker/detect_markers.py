689597dd4b068312a0c8f1282e8cb7ac16ab96bc
ChingT <j60626j@gmail.com>
2019-02-12 16:37:33 +0100

    adapt Apriltag

--- pupil_src/shared_modules/marker_tracker_3d/worker/detect_markers.py
+++ pupil_src/shared_modules/marker_tracker_3d/worker/detect_markers.py
@@ -5,13 +5,12 @@ detector = apriltag.Detector()
 
 
 def detect(frame):
-    detections = detector.detect(frame.gray)
-
+    apriltag_detections = detector.detect(frame.gray)
     marker_detections = {
-        detection.tag_id: {
-            "verts": detection.corners[::-1],
-            "centroid": detection.center / [frame.width, frame.height],
+        apriltag_detection.tag_id: {
+            "verts": apriltag_detection.corners[::-1],
+            "centroid": apriltag_detection.center / [frame.width, frame.height],
         }
-        for detection in detections
+        for apriltag_detection in apriltag_detections
     }
     return marker_detections
